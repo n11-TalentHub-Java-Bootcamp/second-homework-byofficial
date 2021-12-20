ac14168f836bf257874a1718cca8ba9b338883fc
Burak <burak.yildiz0@outlook.com>
2021-12-20 13:59:42 +0300

    Create spring boot project

---
+++ src/main/java/com/burakyildiz/springboot/SpringBootHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.burakyildiz.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHomeworkApplication.class, args);
+	}
+
+}
