e548f968247501ebed2342531bef7de0df297c53
Burak <burak.yildiz0@outlook.com>
2021-12-21 15:15:36 +0300

    Add feature exceptions to review-controller

--- src/main/java/com/burakyildiz/springboot/api/controllers/ReviewsController.java
+++ src/main/java/com/burakyildiz/springboot/api/controllers/ReviewsController.java
@@ -1,8 +1,15 @@
 package com.burakyildiz.springboot.api.controllers;
 
+import com.burakyildiz.springboot.business.concretes.ProductManager;
 import com.burakyildiz.springboot.business.concretes.ReviewManager;
+import com.burakyildiz.springboot.business.concretes.UserManager;
+import com.burakyildiz.springboot.business.exceptions.ProductNotFoundException;
+import com.burakyildiz.springboot.business.exceptions.ReviewNotFoundException;
+import com.burakyildiz.springboot.business.exceptions.UserNotFoundException;
 import com.burakyildiz.springboot.business.mapping.ReviewMapper;
+import com.burakyildiz.springboot.entities.concretes.Product;
 import com.burakyildiz.springboot.entities.concretes.ProductReview;
+import com.burakyildiz.springboot.entities.concretes.User;
 import com.burakyildiz.springboot.entities.dtos.ProductReviewDto;
 import com.burakyildiz.springboot.entities.dtos.ReviewDto;
 import com.burakyildiz.springboot.entities.dtos.UserReviewDto;
@@ -20,6 +27,12 @@ public class ReviewsController {
     @Autowired
     private ReviewManager reviewManager;
 
+    @Autowired
+    private UserManager userManager;
+
+    @Autowired
+    private ProductManager productManager;
+
     @GetMapping("")
     public List<ReviewDto> findAll() {
         List<ProductReview> productReviewList = reviewManager.findAll();
@@ -34,17 +47,38 @@ public class ReviewsController {
     public List<UserReviewDto> findAllUserReviewsList(@PathVariable Long id) {
 
         List<ProductReview> productReviewList = reviewManager.findAllUserReviewList(id);
+        User user = userManager.findById(id);
 
         List<UserReviewDto> reviewDtoList = ReviewMapper.INSTANCE.convertAllProductReviewListToUserReviewDtoList(productReviewList);
+
+        if (user != null) {
+            if (reviewDtoList.size() == 0) {
+                throw new ReviewNotFoundException(user.getFirstName() + " " + user.getLastName() +
+                        " Kullanıcı henüz bir yorum yazmamıştır");
+            }
+        } else {
+            throw new UserNotFoundException("User not found. id: " + id);
+        }
 
         return reviewDtoList;
     }
 
     @GetMapping("/product/{id}")
     public List<ProductReviewDto> findAllProductReviewsList(@PathVariable Long id) {
+        Product product = productManager.findById(id);
         List<ProductReview> productReviewList = reviewManager.findAllProductReviewList(id);
 
         List<ProductReviewDto> reviewDtoList = ReviewMapper.INSTANCE.convertAllProductReviewListToProductReviewDtoList(productReviewList);
+
+        if (product != null) {
+            if (reviewDtoList.size() == 0) {
+                throw new ReviewNotFoundException(product.getProductName() +
+                        " adlı ürüne henüz bir yorum yazmamıştır");
+            }
+        } else {
+            throw new ProductNotFoundException("Product not found. id: " + id);
+        }
+
 
         return reviewDtoList;
     }
