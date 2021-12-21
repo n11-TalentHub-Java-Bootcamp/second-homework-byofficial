b5204e0a81bf37acaa5d88d6fc46001fe0dc5654
Burak <burak.yildiz0@outlook.com>
2021-12-21 17:55:03 +0300

    Refactor spring application

--- src/main/java/com/burakyildiz/springboot/SpringBootHomeworkApplication.java
+++ src/main/java/com/burakyildiz/springboot/SpringBootHomeworkApplication.java
@@ -1,7 +1,5 @@
 package com.burakyildiz.springboot;
 
-import com.burakyildiz.springboot.business.concretes.CategoryManager;
-import com.burakyildiz.springboot.business.concretes.ProductManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,12 +7,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class SpringBootHomeworkApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootHomeworkApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootHomeworkApplication.class, args);
 
-		CategoryManager categoryManager = applicationContext.getBean(CategoryManager.class);
-		ProductManager productManager = applicationContext.getBean(ProductManager.class);
-	}
+    }
 
 }
